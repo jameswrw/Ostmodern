a8bee3d755892d9dea22dd2646cc2ae349fa1210
James Weatherley <james.weatherley@citrix.com>
2017-11-17 19:19:11 +0000

    Download data successfully - needed to add an entry to info.plist to allow downloads from http (i.e. not https) URLs. Parse the returned json, and populate an array of APISet objects, and return to the UI level via the completion handler.

--- ostest/API.swift
+++ ostest/API.swift
@@ -15,68 +15,79 @@ import SwiftyBeaver
  The API Class connects to the Skylark API to access content and present it type safe structs
  */
 class API {
-  
-  /// Singleton instance
-  static let instance = API()
-  
-  /// Log
-  let log = SwiftyBeaver.self
-  
-  /// The base URL
-  let baseURL = "http://feature-code-test.skylark-cms.qa.aws.ostmodern.co.uk:8000"
-  
-  /**
-   Get sets
-   */
-  func getSets (completion : @escaping (_ isSuccess : Bool, _ set : [APISet]?) -> Void) {
     
-    let apiString = "\(baseURL)/api/sets/"
-    log.verbose("Getting sets with URL \(apiString)")
+    /// Singleton instance
+    static let instance = API()
     
-    /// Request
-    Alamofire.request(apiString).responseJSON { response in
-      
-      self.log.verbose("Response for getting sets \(response.response.debugDescription)")
-      
-      if let _ = response.result.value {
-        completion(false, nil)
-      }
-    }
+    /// Log
+    let log = SwiftyBeaver.self
     
-  }
-  
-  /**
-   Updates an APISet object from the /sets/ endpoint to a full formed APISet with correct images
-   
-   - parameter set: The APISet to convert
-   - returns: APISet
-   */
-  func updateSet (set : APISet, completion : @escaping (_ isSuccess : Bool, _ set : APISet?) -> Void) {
+    /// The base URL
+    let baseURL = "http://feature-code-test.skylark-cms.qa.aws.ostmodern.co.uk:8000"
     
-    guard let apiString = set.imageURLs.first else {
-      return
-    }
-    log.verbose("Getting image information with URL \(apiString)")
+    // For checking if the API request was successful.
+    let kHTTPSuccess = 200
     
-    
-    /// Request
-    Alamofire.request("\(self.baseURL)\(apiString)").responseJSON { response in
-      
-      self.log.verbose("Response for getting set image \(response.response.debugDescription)")
-      
-      if let result = response.result.value {
-        let json = JSON(result)
-        guard let url = json["url"].string else {
-          completion(false, nil)
-          return
+    /**
+     Get sets
+     */
+    func getSets (completion : @escaping (_ isSuccess : Bool, _ set : [APISet]?) -> Void) {
+        
+        let apiString = "\(baseURL)/api/sets/"
+        log.verbose("Getting sets with URL \(apiString)")
+        
+        /// Request
+        Alamofire.request(apiString).responseJSON { response in
+            
+            var sets: [APISet]?
+            defer {
+                completion(sets != nil ? true : false, sets)
+            }
+            
+            self.log.verbose("Response for getting sets \(response.response.debugDescription)")
+            
+            if let httpResponse = response.response {
+                if httpResponse.statusCode == self.kHTTPSuccess {
+                    // Perhaps value! is a bit fast and loose, but I'm assuming that an HTML 200 code means we're good to go...
+                    let json = JSON(response.result.value!)
+                    sets = APISet.parse(json)
+                }
+            }
         }
-        
-        let newSet = APISet(uid: set.uid, title: set.title, setDescription: set.setDescription, setDescriptionFormatted: set.setDescriptionFormatted, summary: set.summary, imageURLs: [url])
-        completion(true, newSet)
-        
-      } else {
-        completion(false, nil)
-      }
     }
-  }
+    
+    /**
+     Updates an APISet object from the /sets/ endpoint to a full formed APISet with correct images
+     
+     - parameter set: The APISet to convert
+     - returns: APISet
+     */
+    func updateSet (set : APISet, completion : @escaping (_ isSuccess : Bool, _ set : APISet?) -> Void) {
+        
+        guard let apiString = set.imageURLs.first else {
+            return
+        }
+        log.verbose("Getting image information with URL \(apiString)")
+        
+        
+        /// Request
+        Alamofire.request("\(self.baseURL)\(apiString)").responseJSON { response in
+            
+            self.log.verbose("Response for getting set image \(response.response.debugDescription)")
+            
+            if let result = response.result.value {
+                let json = JSON(result)
+                guard let url = json["url"].string else {
+                    completion(false, nil)
+                    return
+                }
+                
+                let newSet = APISet(uid: set.uid, title: set.title, setDescription: set.setDescription, setDescriptionFormatted: set.setDescriptionFormatted, summary: set.summary, imageURLs: [url])
+                completion(true, newSet)
+                
+            } else {
+                completion(false, nil)
+            }
+        }
+    }
 }
